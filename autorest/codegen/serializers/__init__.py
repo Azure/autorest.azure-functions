b98be4d2f36c35f492d2a14f7f4c90b7440bcd37
Chad Kittel <chad.kittel@gmail.com>
2020-07-27 09:04:54 -0500

    Initial work for csharp, lots of placeholders/commented out code (#5)
    * update azure-tools/extension version
    
    * Introduce CSharp as option, currently generates python :P
    
    * emit hard coded host file (get plumbing working)
    
    * add gitignore and fix host configuration
    
    * Add local.settings.json (hard coded for now)
    
    * Add csproj stub

--- autorest/codegen/serializers/__init__.py
+++ autorest/codegen/serializers/__init__.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 from .azure_functions import Languages
 from .azure_functions.python import AzureFunctionsPythonSerializer
+from .azure_functions.csharp import AzureFunctionsCSharpSerializer
 from ..models import CodeModel
 from ...jsonrpc import AutorestAPI
 
@@ -35,9 +36,19 @@ class JinjaSerializer:
             Path(".") if code_model.options["no_namespace_folders"]
             else Path(*(code_model.namespace.split("."))))
 
-        if code_model.options['language'] == Languages.PYTHON.value:
+        language = code_model.options['language']
+
+        if language == Languages.PYTHON.value:
             _LOGGER.debug("AzureFunctionsPythonSerializer serializing now")
             serializer = AzureFunctionsPythonSerializer(
+                    autorest_api=self._autorestapi,
+                    code_model=code_model,
+                    function_app_path=function_app_path,
+                    async_mode=False)
+            serializer.serialize()
+        elif language == Languages.CSHARP.value:
+            _LOGGER.debug("AzureFunctionsCSharpSerializer serializing now")
+            serializer = AzureFunctionsCSharpSerializer(
                     autorest_api=self._autorestapi,
                     code_model=code_model,
                     function_app_path=function_app_path,
